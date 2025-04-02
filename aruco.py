999262dcbe8e2a58d39796eda7261e6010ca155c
aky <akiyugata271@gmail.com>
2025-04-02 21:55:39 +0900

    できた

---
+++ aruco.py
@@ -0,0 +1,27 @@
+
+import cv2
+import numpy as np
+from cv2 import aruco
+
+# get dicionary and get parameters
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters()
+
+cap = cv2.VideoCapture(0)
+while(cap.isOpened):
+    ret, frame = cap.read()
+    if True == ret:
+        corners, ids, rejectedCandidates = aruco.detectMarkers(frame, dictionary, parameters=parameters)
+        #print(ids)
+        print(corners)
+        aruco.drawDetectedMarkers(frame, corners, ids)
+        cv2.imshow("webcam", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
